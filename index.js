d59c4daa55d26a326cdc5628b8693e6196b28c30
Santos Lopez <agus77chang@gmail.com>
2025-05-30 10:12:40 -0600

    1-Inicializando mecanografo

---
+++ index.js
@@ -0,0 +1,24 @@
+const entrada = document.querySelector('.entrada')
+const valor = document.querySelector('.escribir')
+const display = document.querySelector('.display')
+entrada.addEventListener('keyup', () => {
+    comprobacion(entrada.value)
+})
+
+
+
+const comprobacion = (valorentrada) => {
+    let compt = valor.textContent
+    display.textContent = valorentrada
+    if (compt.startsWith(valorentrada)) {
+        //display.classList.remove("bg-danger")
+        //display.classList.add("bg-success")
+        if (compt.length == String(valorentrada).length) {
+            alert("Terminado!!")
+        }
+    }
+    else {
+        //display.classList.remove("bg-success")
+        //display.classList.add("bg-danger")
+    }
+}
